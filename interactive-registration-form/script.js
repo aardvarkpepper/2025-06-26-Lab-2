af392b1ffa7ebecd22b53ea1f04d017a950e12be
James Lee <aardvarkpepper@gmail.com>
2025-06-27 08:20:11 -0400

    Add final validation to form.

--- interactive-registration-form/script.js
+++ interactive-registration-form/script.js
@@ -49,48 +49,62 @@ const passwordInput = document.getElementById('password');
 const passwordError = document.getElementById('passwordError');
 const confirmPasswordInput = document.getElementById('confirmPassword');
 const confirmPasswordError = document.getElementById('confirmPasswordError');
+
 const registrationForm = document.getElementById('registrationForm');
+const finalConfirm = document.getElementById('finalConfirm');
 
 const removeUsernameFromStorage = document.getElementById('removeUsernameFromStorage');
 
 let usernamestored = null;
+let finalValidation = {username: false, email: false, password: false, confirmpassword: false};
 
 usernameInput.addEventListener('input', (event) => {
   //console.log('username aEL triggered');
   // We are explicitly instructed to use 'input'. Selecting the field then moving away, or even pressing enter, or pressing submit on the form, does not trigger this event listener.  Only when a character is entered does this trigger.  I suppose I could put in an event listener for blur or something too but eh.
   if (usernameInput.validity.valueMissing) {
+    finalValidation.username = false;
     usernameInput.setCustomValidity(`Please enter a user name, like 'ranchpizzalover1'`);
   } else {
+    finalValidation.username = true;
     usernameInput.setCustomValidity('');
   }
   usernameError.textContent = usernameInput.validationMessage;
 });
 emailInput.addEventListener('input', (event) => {
   if (emailInput.validity.valueMissing) {
+    finalValidation.email = false;
     emailInput.setCustomValidity(`Please enter an email, like 'ranchpizzalover1@pizzalovers.com'`);
   } else if (emailInput.validity.typeMismatcch) {
+    finalValidation.email = false;
     emailInput.setCustomValidity('Please enter a valid email address.')
   } else {
+    finalValidation.email = true;
     emailInput.setCustomValidity('');
   }
   emailError.textContent = emailInput.validationMessage;
 });
 passwordInput.addEventListener('input', (event) => {
   if (passwordInput.validity.valueMissing) {
+    finalValidation.password = false;
     passwordInput.setCustomValidity(`Please enter a password at least 8 characters long, including an uppercase, lowercase, and number.`);
   } else if (passwordInput.validity.tooShort) {
+    finalValidation.password = false;
     passwordInput.setCustomValidity('Please enter a password of at least 8 characters.');
   } else if (passwordInput.validity.patternMismatch) {
-    passwordInput.setCustomValidity('Pleae enter a password with at least one uppercase, one lowercase, and one number.')
+    finalValidation.password = false;
+    passwordInput.setCustomValidity('Pleae enter a password with at least one uppercase, one lowercase, and one number.');
   } else {
+    finalValidation.password = true;
     passwordInput.setCustomValidity('');
   }
   passwordError.textContent = passwordInput.validationMessage;
 });
 confirmPasswordInput.addEventListener('input', (event) => {
   if (confirmPasswordInput.value !== passwordInput.value) {
+    finalValidation.confirmpassword = false;
     confirmPasswordInput.setCustomValidity('Please re-enter password; passwords do not currently match.')
   } else {
+    finalValidation.confirmpassword = true;
     confirmPasswordInput.setCustomValidity('');
   }
   confirmPasswordError.textContent = confirmPasswordInput.validationMessage;
@@ -98,7 +112,11 @@ confirmPasswordInput.addEventListener('input', (event) => {
 
 registrationForm.addEventListener('submit', (event) => {
   event.preventDefault();
-  // send to localstorage.
+  if (finalValidation.username && finalValidation.email && finalValidation.password && finalValidation.confirmpassword) {
+    finalConfirm.textContent = "Input OK."
+  } else {
+    finalConfirm.textContent = "Input not valid."
+  }
 });
 
 removeUsernameFromStorage.addEventListener('click', (event) => {
